00eca6e210ab5dba0001d6722711871fe932ed7f
rana <rana@192.168.0.7>
2018-02-19 19:41:05 +0530

    First Commit:- Added pom and created simple springBoot project.

---
+++ src/main/java/system_ordering/controller/MyController.java
@@ -0,0 +1,15 @@
+package system_ordering.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@RequestMapping("/")
+	public String getName() {
+		return "abc";
+
+	}
+
+}
